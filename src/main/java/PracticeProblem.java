2630407083e6616e2733facb01196bbac3187890
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-03-10 15:01:25 +0000

    buh

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,27 @@ public class PracticeProblem {
 	}
 
 	//Create your functions here
+	public static int sum(int num1, int num2){
+		int sum;
+		sum = num1 + num2;
+		return sum;	}
+		
+
+	public static int difference(int num1, int num2){
+		int difference;
+		difference = num1 - num2;
+		return difference;	}
+
+	public static double product(double num1, double num2){
+		double product;
+		product = num1 * num2;
+		return product;	}
+
+	public static String removeFirst(String buh){
+
+		String huh;
+		huh = buh.substring(1);
+		return huh;
+	}
 
 }
